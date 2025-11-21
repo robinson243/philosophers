c17a02144fa0bdd16553ac56b846ea9065d734fc
Robinson Mukena Zevo <romukena@f1r11s17.clusters.42paris.fr>
2025-11-21 16:27:47 +0100

    finish i think

--- philo/monitors.c
+++ philo/monitors.c
@@ -6,7 +6,7 @@
 /*   By: romukena <romukena@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/18 15:45:18 by romukena          #+#    #+#             */
-/*   Updated: 2025/11/20 01:57:27 by romukena         ###   ########.fr       */
+/*   Updated: 2025/11/21 16:27:00 by romukena         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,30 +27,49 @@ void	clean_simulation(t_args *args, t_philo *philos)
 	free(philos);
 }
 
+int	check_death_and_count(t_args *args, int *full)
+{
+	long	last;
+	int		i;
+
+	i = 0;
+	while (i < args->philo_count)
+	{
+		pthread_mutex_lock(&args->print_mutex);
+		last = args->philos[i].last_meal;
+		if (!args->finished && get_time() - last > args->time_to_die)
+		{
+			printf("%ld philosopher [%d] died\n", get_time() - args->start_time,
+				args->philos[i].id);
+			args->finished = 1;
+			pthread_mutex_unlock(&args->print_mutex);
+			return (1);
+		}
+		if (args->must_eat > 0 && args->philos[i].eat_count < args->must_eat)
+			*full = 0;
+		pthread_mutex_unlock(&args->print_mutex);
+		i++;
+	}
+	return (0);
+}
+
 void	*monitor_philosophers(void *arg)
 {
 	t_args	*args;
-	int		i;
-	long	last;
+	int		full;
 
 	args = (t_args *)arg;
 	while (1)
 	{
-		i = 0;
-		while (i < args->philo_count)
+		full = 1;
+		if (check_death_and_count(args, &full))
+			return (NULL);
+		if (args->must_eat > 0 && full == 1)
 		{
 			pthread_mutex_lock(&args->print_mutex);
-			last = args->philos[i].last_meal;
-			if (!args->finished && get_time() - last > args->time_to_die)
-			{
-				printf("%ld philosopher [%d] died\n", get_time()
-					- args->start_time, args->philos[i].id);
-				args->finished = 1;
-				pthread_mutex_unlock(&args->print_mutex);
-				return (NULL);
-			}
+			args->finished = 1;
 			pthread_mutex_unlock(&args->print_mutex);
-			i++;
+			return (NULL);
 		}
 		usleep(1000);
 	}
--- philo/philosophers.c
+++ philo/philosophers.c
@@ -6,7 +6,7 @@
 /*   By: romukena <romukena@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/18 15:44:02 by romukena          #+#    #+#             */
-/*   Updated: 2025/11/20 22:41:54 by romukena         ###   ########.fr       */
+/*   Updated: 2025/11/21 16:25:29 by romukena         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -46,7 +46,7 @@ void	philo_take_forks(t_philo *philo)
 	pthread_mutex_unlock(philo->left_fork);
 	pthread_mutex_unlock(philo->right_fork);
 } */
-void	philo_eat(t_philo *philo)
+/*void	philo_eat(t_philo *philo)
 {
 	pthread_mutex_lock(&philo->args->print_mutex);
 	if (!philo->args->finished)
@@ -58,6 +58,19 @@ void	philo_eat(t_philo *philo)
 	philo->last_meal = get_time();
 	philo->eat_count += 1;
 	pthread_mutex_unlock(&philo->args->print_mutex);
+	pthread_mutex_unlock(philo->left_fork);
+	pthread_mutex_unlock(philo->right_fork);
+}*/
+void	philo_eat(t_philo *philo)
+{
+	pthread_mutex_lock(&philo->args->print_mutex);
+	if (!philo->args->finished)
+		printf("%ld philosopher [%d] is eating\n", get_time()
+			- philo->args->start_time, philo->id);
+	philo->last_meal = get_time();
+	philo->eat_count++;
+	pthread_mutex_unlock(&philo->args->print_mutex);
+	ft_usleep(philo->args->time_to_eat, philo);
 	pthread_mutex_unlock(philo->left_fork);
 	pthread_mutex_unlock(philo->right_fork);
 }
@@ -86,6 +99,7 @@ void	*philo_life(void *arg)
 	t_philo	*philo;
 
 	philo = (t_philo *)arg;
+	philo->last_meal = get_time();
 	if (philo->args->philo_count == 1)
 	{
 		philo_think(philo);
